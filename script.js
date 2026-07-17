ca10bfacde5e32c74e57c71f80d6224e5042779c
hamokshaxavi-crypto <hamokshaxavi@gmail.com>
2026-07-17 04:37:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,155 +1,44 @@
-let taskInput = document.getElementById("taskInput");
+let productName = document.getElementById("productName");
+let productPrice = document.getElementById("productPrice");
+let productCategory = document.getElementById("productCategory");
+let tbody = document.getElementById("tbody");
 let addBtn = document.getElementById("addBtn");
-let taskList = document.getElementById("taskList");
-
-let total = document.getElementById("total");
-let completed = document.getElementById("completed");
-
-let deleteAll = document.getElementById("deleteAll");
-
-let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-
-renderTasks();
-
-addBtn.addEventListener("click", addTask);
-
-taskInput.addEventListener("keypress", function(e){
-    if(e.key==="Enter"){
-        addTask();
-    }
-});
-
-function addTask(){
-
-    let text = taskInput.value.trim();
-
-    if(text==""){
-        alert("Please Enter Task");
+let updateBtn = document.getElementById("updateBtn");
+let products = [];
+addBtn.addEventListener("click", function() {
+    if (productName.value === "" || productPrice.value === "" || productCategory.value === "") {
+        alert("Please fill in all fields");
         return;
     }
-
-    let task = {
-        title:text,
-        done:false
-    };
-
-    tasks.push(task);
-
-    saveData();
-
-    renderTasks();
-
-    taskInput.value="";
-}
-
-function renderTasks(){
-
-    taskList.innerHTML="";
-
-    tasks.forEach(function(task,index){
-
-        let li=document.createElement("li");
-
-        let span=document.createElement("span");
-
-        span.innerText=task.title;
-
-        span.classList.add("task");
-
-        if(task.done){
-            span.classList.add("completed");
-        }
-
-        span.addEventListener("click",function(){
-
-            tasks[index].done=!tasks[index].done;
-
-            saveData();
-
-            renderTasks();
-
-        });
-
-        let actions=document.createElement("div");
-
-        actions.classList.add("actions");
-
-        let editBtn=document.createElement("button");
-
-        editBtn.innerText="Edit";
-
-        editBtn.classList.add("edit");
-
-        editBtn.addEventListener("click",function(){
-
-            let newTask=prompt("Edit Task",task.title);
-
-            if(newTask!=null && newTask.trim()!=""){
-
-                tasks[index].title=newTask;
-
-                saveData();
-
-                renderTasks();
-
-            }
-
-        });
-
-        let deleteBtn=document.createElement("button");
-
-        deleteBtn.innerText="Delete";
-
-        deleteBtn.classList.add("delete");
-
-        deleteBtn.addEventListener("click",function(){
-
-            tasks.splice(index,1);
-
-            saveData();
-
-            renderTasks();
-
-        });
-
-        actions.appendChild(editBtn);
-
-        actions.appendChild(deleteBtn);
-
-        li.appendChild(span);
-
-        li.appendChild(actions);
-
-        taskList.appendChild(li);
-
-    });
-
-    total.innerText=tasks.length;
-
-    let count=tasks.filter(task=>task.done).length;
-
-    completed.innerText=count;
-
-}
-
-deleteAll.addEventListener("click",function(){
-
-    let check=confirm("Delete All Tasks?");
-
-    if(check){
-
-        tasks=[];
-
-        saveData();
-
-        renderTasks();
-
+    let newProduct={
+        name: productName.value,
+        price: productPrice.value,
+        category: productCategory.value
     }
-
+    console.log(newProduct);
+  products.push(newProduct);
+  console.log(products);
+   
+    clearInputs();
+    rendertable();
 });
 
-function saveData(){
-
-    localStorage.setItem("tasks",JSON.stringify(tasks));
-
+function rendertable() {
+    tbody.innerHTML = "";
+    for (let i = 0; i < products.length; i++) {
+        let row = `<tr>
+        <td>${products[i].name}</td>
+        <td>${products[i].price}</td>
+        <td>${products[i].category}</td>
+        <td><button class="edit-Btn" data-index="${i}">Edit</button></td>
+        <td><button class="delete-Btn" data-index="${i}">Delete</button></td>
+        </tr>`;
+        tbody.innerHTML += row;
+    }
 }
+function clearInputs() {
+    productName.value = "";
+    productPrice.value = "";
+    productCategory.value = "";
+}
+
